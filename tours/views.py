625a8c07a61091036d1f3119a578d3e526a146eb
Realmarvin17 <50673740+Realmarvin17@users.noreply.github.com>
2020-07-31 17:09:16 +0300

    Initial commit

---
+++ tours/views.py
@@ -0,0 +1,70 @@
+from random import sample
+
+from django.http import HttpResponseNotFound, Http404, HttpResponseServerError
+from django.shortcuts import render
+from django.views import View
+
+import tours.data as data
+
+
+class MainView(View):
+    @staticmethod
+    def get(request):
+        sample_tours = sample(data.tours.items(), 6)
+
+        return render(request, 'tours/index.html', {'title': data.title,
+                                                    'subtitle': data.subtitle,
+                                                    'description': data.description,
+                                                    'departures': data.departures,
+                                                    'sample_tours': sample_tours})
+
+
+class DepartureView(View):
+    @staticmethod
+    def get(request, departure):
+        if departure not in data.departures:
+            raise Http404
+
+        departure_title = data.departures[departure]
+
+        tours_from_departure = {}
+        for tour_id, tour in data.tours.items():
+            if tour['departure'] == departure:
+                tours_from_departure[tour_id] = tour
+
+        tours_count = len(tours_from_departure)
+        tours_prices = sorted(tour['price'] for tour in tours_from_departure.values())
+        tours_nights = sorted(tour['nights'] for tour in tours_from_departure.values())
+
+        return render(request, 'tours/departure.html', {'title': data.title,
+                                                        'departures': data.departures,
+                                                        'departure_title': departure_title,
+                                                        'tours_count': tours_count,
+                                                        'min_price_tour': tours_prices[0],
+                                                        'max_price_tour': tours_prices[-1],
+                                                        'min_nights_tour': tours_nights[0],
+                                                        'max_nights_tour': tours_nights[-1],
+                                                        'tours_from_departure': tours_from_departure})
+
+
+class TourView(View):
+    @staticmethod
+    def get(request, tour_id):
+        if tour_id not in data.tours:
+            raise Http404
+
+        tour_data = data.tours[tour_id]
+        departure_title = data.departures[tour_data['departure']]
+
+        return render(request, 'tours/tour.html', {'title': data.title,
+                                                   'departures': data.departures,
+                                                   'departure_title': departure_title,
+                                                   'tour': tour_data})
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Ой, что то сломалось... Простите извините!')
+
+
+def custom_handler500(request, exception):
+    return HttpResponseServerError('Сервер не отвечает... Скоро починим!')
